37029f0504b47dee0be0a3fa2205f6359387894c
Sergio <sergio.munoz@upm.es>
2021-10-06 10:17:04 +0200

    added readme and solution

---
+++ solucion.py
@@ -0,0 +1,119 @@
+import string
+
+RATE = 44471.78
+
+def euros_a_bitcoins(euros):
+  return round(euros / RATE, 2)
+
+def bitcoins_a_euros(bitcoins):
+  return bitcoins * RATE
+
+def contar_vocales(texto):
+  contador = 0
+  for letra in texto:
+    if letra.lower() in "aeiouáéíóú":
+      contador += 1
+  return contador
+
+def es_palindromo(text):
+  text = str.lower(text).replace(" ", "")
+  return text == ''.join(reversed(text))
+
+def max_temperaturas(temperaturas, umbral):
+  max_temps = []
+  for temp in temperaturas:
+    if temp > umbral:
+      max_temps.append(temp)
+  return max_temps
+
+def cifrar(texto, desplazamiento):
+  '''Transforma un texto dado usando cifrado César con un desplazamiento dado.'''
+  texto = texto.upper()
+  CODE = 'ABCDEFGHIJKLMÑOPQRSTUVWXYZÁÉÍÓÚ'
+  cifrado = ""
+  for c in texto:
+      if c in CODE:
+          c = CODE[(CODE.index(c) + desplazamiento) % len(CODE)]
+      cifrado += c
+  return cifrado
+
+
+def descifrar(cifrado, desplazamiento):
+  '''
+  Aplicada a un texto cifrado con el mismo desplazamiento,
+  devuelve el texto original.
+  '''
+  return cifrar(cifrado, -desplazamiento)
+
+
+mis_productos = []
+
+def insertar(producto):
+  mis_productos.append(producto)
+
+def borrar(numero):
+  del mis_productos[numero]
+
+def lista_productos():
+  return mis_productos
+
+def productos():
+  if not mis_productos:
+    print('No hay productos')
+    return
+  for ix, producto in enumerate(mis_productos):
+    print(f'{ix}: {producto}')
+
+def cantidad():
+  return len(mis_productos)
+
+
+def menu():
+    print('Menú interactivo')
+    print()
+    while True:
+      tokens = input('-> ').split(" ", 1)
+      accion = tokens[0]
+      if accion == 'convertir':
+        src, dst, qty = tokens[1].split()
+        if src == 'euros' and dst == 'bitcoins':
+          res = euros_a_bitcoins(float(qty))
+        elif src == 'bitcoins' and dst == 'euros':
+          res = bitcoins_a_euros(float(qty))
+        else:
+          res = 'Monedas no reconocidas'
+        print(res)
+      elif accion == 'palindromo':
+          palindromo = es_palindromo(tokens[1])
+          print(palindromo)
+      elif accion == 'contar':
+          cuenta = contar_vocales(tokens[1])
+          print(cuenta)
+      elif accion == 'temperaturas':
+        tokens = tokens[1].split()
+        temperaturas = tokens[0]
+        umbral = tokens[1]
+        print(max_temperaturas(temperaturas, umbral))
+      elif accion == 'productos':
+          if len(tokens) < 2:
+            productos()
+            continue
+          tokens = tokens[1].split()
+          sub = tokens[0]
+          if sub == 'nuevo':
+            insertar(tokens[1])
+          elif sub == 'borrar':
+            borrar(int(tokens[1]))
+      elif accion == 'salir':
+        return
+      else:
+        print("acción no reconocida")
+
+if __name__ == '__main__':
+    hm = cifrar('hola mundo', 10)
+    print(hm, descifrar(hm, 10), 10)
+    hm = cifrar('hola mundo', 15)
+    print(hm, descifrar(hm, 15), 15)
+    hm = cifrar('adiós, mundo!', 20)
+    print(hm, descifrar(hm, 20), 20)
+    menu()
---
+++ test.py
@@ -0,0 +1,95 @@
+import unittest
+import sys
+import io
+from contextlib import contextmanager
+
+from solucion import *
+
+
+@contextmanager
+def redirected():
+    '''
+    Context manager para capturar las salida de print (sys.stdout en general).
+    Nos va a permitir capturar la salida por pantalla de las funciones del ejercicio.
+
+    Sería mejor separar el código de mostrar_tareas en dos funciones: una que devuelve el
+    texto, y otra que llama a la primera y lo muestra. Así no haría falta este tipo de
+    programación avanzada.
+    '''
+    saved = sys.stdout
+    out = io.StringIO()
+    sys.stdout = out
+    try:
+        yield out
+    finally:
+        sys.stdout = saved
+
+
+def red(fun, *args, **kwargs):
+    with redirected() as out:
+        fun(*args, **kwargs)
+    return out.getvalue()
+
+
+class TestUtilidades(unittest.TestCase):
+
+    '''
+    def test_cifrar(self):
+        assert cifrar('hola mundo', 0) == 'HOLA MUNDO'
+        assert cifrar('hola mundo', 20) == 'ÉDAU BJNXD'
+        assert cifrar('adiós, mundo', 20) == 'UXÍSH, BJNXD'
+
+    def test_descifrar(self):
+        assert descifrar('UXÍSH, BJNXD', 20) ==  'ADIÓS, MUNDO'
+        assert descifrar('ÉDAU BJNXD', 20) == 'HOLA MUNDO'
+    '''
+
+    def test_convertir(self):
+        assert (euros_a_bitcoins(10000) - 0.22) < 0.1
+        assert (bitcoins_a_euros(2) - 89140.34) < 1
+
+    def test_contar(self):
+        assert contar_vocales('esto son ocho vocales') == 8
+        assert contar_vocales('  3 vocales ') == 3
+
+    def test_palindromo(self):
+        assert es_palindromo('ella te da detalle') == True
+        assert es_palindromo('  Este texto no es un palindromo ') == False
+
+    def test_temperaturas(self):
+        assert max_temperaturas([12.0, 3.0, 4.0, 17.0, 26.0], 11) == [12.0, 17.0, 26.0]
+        assert max_temperaturas([12.0, 3.0, 4.0, 17.0, 26.0], 32) == []
+
+    def test_productos_vacios(self):
+        lista_productos().clear()
+        v = red(productos)
+        assert 'No hay productos' in v
+        assert len(lista_productos()) == 0
+
+    def test_insertar(self):
+        insertar('prueba')
+        insertar('final')
+        assert len(lista_productos()) == 2
+        assert lista_productos()[0] == 'prueba'
+        assert lista_productos()[1] == 'final'
+
+    def test_borrar(self):
+        lista_productos().clear()
+        insertar('prueba')
+        insertar('final')
+        borrar(1)
+        assert len(lista_productos()) == 1
+        borrar(0)
+        assert len(lista_productos()) == 0
+
+    def test_mostrar_productos(self):
+        lista_productos().clear()
+        v = red(productos)
+        assert 'No hay productos' in v
+        assert ':' not in v
+        insertar('prueba')
+        v = red(productos)
+        assert 'prueba' in v
+
+if __name__ == '__main__':
+    unittest.main(verbosity=0)
